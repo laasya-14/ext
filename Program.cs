de5eb034675ac94b18175723b7a07ae70cefbc1f
Laasya Kothapalli <t-skothapall@microsoft.com>
2025-01-02 13:49:06 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Astred.GraphLib;
+
+public class ExternalDep
+{
+    private static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        string digestpath = @"C:\Users\t-skothapall\code\RustTranslation\samples\sample\.astred.digest.zip";
+        var codegraph = CodeGraph.BuildFromDigest(digestpath);
+        codegraph.PrintGraph(includeAst:false, showRelations:true);
+
+    }
+    // astred.project.json file --> builds the codegraph (or symbolic graph?)
+    // gets the list of all external dependencies and prints them to the user
+}
